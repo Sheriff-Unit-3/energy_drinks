188e31abbb267265e8d7f6c6e4f0f4fc13e5d42f
Sheriff_U3 <sheriff_u3@noreply.codeberg.org>
2026-06-13 18:53:42 -0500

    Add purple energy drink and new names

--- init.lua
+++ init.lua
@@ -4,12 +4,13 @@ energy_drinks.S, energy_drinks.PS = core.get_translator("energy_drinks")
 local S = energy_drinks.S
 
 energy_drinks.drinks = {
-  red = S("Red"),
-  blue = S("Blue"),
-  yellow = S("Yellow"),
+  red = S("Strawberry"),
+  blue = S("Blueberry"),
+  yellow = S("Lemon"),
   orange = S("Orange"),
-  green = S("Green"),
-  white = S("White")
+  green = S("Green Apple"),
+  white = S("Icy Snow"),
+  purple = S("Grape")
 }
 
 local modpath = core.get_modpath(core.get_current_modname())
